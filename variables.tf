87f1886566850bcd6c3d69307f4ce2ed0e974255
peada04 <dpeacock@hashicorp.com>
2025-05-03 17:43:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "destination_org" {
 
 variable "gh_token" {
   description = "Github token with permissions to create and delete repos."
+  default = "123xyz"
 }
 
 variable "waypoint_application" {
@@ -29,4 +30,5 @@ variable "aws_region" {
 
 variable "route53_zone_id" {
   description = "The premade route53 zone ID. The zone is created outside of here so that the domain config can be set up beforehand."
+  default = "123xyz"
 }
